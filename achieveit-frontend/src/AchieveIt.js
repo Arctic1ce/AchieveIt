0ef188782abc8004eff5b5b2033d6640d5940497
Arctic1ce <davariartin@gmail.com>
2023-10-25 10:47:07 -0700

    cleaned up unnecessary files

---
+++ achieveit-frontend/src/AchieveIt.js
@@ -0,0 +1,14 @@
+// AchieveIt.js
+
+function AchieveIt() {
+  return (
+    <div className="AchieveIt">
+        <div className="header">
+            
+        </div>
+      <p>AchieveIt</p>
+    </div>
+  );
+}
+
+export default AchieveIt;
