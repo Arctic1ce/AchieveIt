59bedf816aa7442d39c6f1efab64582be73539a2
Arctic1ce <davariartin@gmail.com>
2023-10-25 16:41:36 -0700

    finished navbar

--- achieveit-frontend/src/AchieveIt.js
+++ achieveit-frontend/src/AchieveIt.js
@@ -1,10 +1,11 @@
 // AchieveIt.js
+import Navbar from './Navbar.js';
 
 function AchieveIt() {
   return (
     <div className="AchieveIt">
         <div className="header">
-            
+            <Navbar/>
         </div>
       <p>AchieveIt</p>
     </div>
