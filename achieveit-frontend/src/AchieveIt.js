43e0446ccdb0dbe77ca32261ef5e4ec2bbd30f07
Arctic1ce <davariartin@gmail.com>
2023-10-26 10:58:24 -0700

    made list of todo lists look better

--- achieveit-frontend/src/AchieveIt.js
+++ achieveit-frontend/src/AchieveIt.js
@@ -6,33 +6,43 @@ import React, { useState, useEffect } from 'react';
 const tasks = [
     {
         name: 'All Tasks',
+        num_items: 43,
     },
     {
         name: 'Favorites',
+        num_items: 4,
     },
     {
         name: 'Groceries',
+        num_items: 7,
     },
     {
         name: 'Work',
+        num_items: 10,
     },
     {
         name: 'School',
+        num_items: 3,
     },
     {
         name: 'Sports',
+        num_items: 2,
     },
     {
         name: 'Cars',
+        num_items: 1,
     },
     {
         name: 'Wishlist',
+        num_items: 5,
     },
     {
         name: 'Something',
+        num_items: 7,
     },
     {
         name: 'Idk',
+        num_items: 4,
     },
 ]
 
--- achieveit-frontend/src/TaskList.js
+++ achieveit-frontend/src/TaskList.js
@@ -4,17 +4,17 @@ import Badge from 'react-bootstrap/Badge';
 import Row from 'react-bootstrap/Row';
 import Tab from 'react-bootstrap/Tab';
 
-function TabsExample(props) {
+function TaskList(props) {
     return (
-        <Tab.Container id="list-group-tabs-example" defaultActiveKey="#link1">
+        <Tab.Container id="list-group-tabs-example" defaultActiveKey={`#link1`}>
             <Row>
-                <Col sm={4}>
+                <Col sm={2}>
                     <ListGroup>
                         {props.lists.map((item) => (
-                            <ListGroup.Item action href={`#${item.name}`}>
+                            <ListGroup.Item key={item.name} action href={`#${item.name}`} className="d-flex justify-content-between align-items-start">
                                 {item.name}
                                 <Badge bg="primary" pill>
-                                    14
+                                    {item.num_items}
                                 </Badge>
                             </ListGroup.Item>
                         ))}
@@ -31,54 +31,4 @@ function TabsExample(props) {
     );
 }
 
-export default TabsExample;
-
-
-
-
-
-// import * as React from 'react';
-// import Box from '@mui/material/Box';
-// import List from '@mui/material/List';
-// import ListItem from '@mui/material/ListItem';
-// import ListItemButton from '@mui/material/ListItemButton';
-// import ListItemText from '@mui/material/ListItemText';
-// //import { FixedSizeList as List } from 'react-window';
-
-// function TaskList(props) {
-//     return (
-//         // <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-//         //     {props.lists.map((list) => (
-//         //         <ListItem key={list.name} component="div" disablePadding>
-//         //             <ListItemButton>
-//         //                 <ListItemText primary={list.name} />
-//         //             </ListItemButton>
-//         //         </ListItem>
-//         //     ))}
-//         // </List>
-//     );
-
-//     return (
-//         <Box
-//             sx={{ width: '100%', height: 400, maxWidth: 400, bgcolor: 'background.paper' }}
-//         >
-//             <List
-//                 height={400}
-//                 width={400}
-//                 itemSize={46}
-//                 itemCount={props.lists.length}
-//                 overscanCount={5}
-//             >
-//                 {props.lists.map((list) => (
-//                     <ListItem key={list.name} component="div" disablePadding>
-//                         <ListItemButton>
-//                             <ListItemText primary={`Item`} />
-//                         </ListItemButton>
-//                     </ListItem>
-//                 ))}
-//             </List>
-//         </Box>
-//     );
-// }
-
-// export default TaskList;
+export default TaskList;
