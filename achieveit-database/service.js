1d85adc160c8d7383896e2d97a9f81fdb29de333
Russ Sobti <russ@recombinantsoftware.com>
2023-11-05 22:23:28 -0800

    probably like halfway working, need to make it all the way working

--- achieveit-database/service.js
+++ achieveit-database/service.js
@@ -1,1 +1,58 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import {TodoList, TodoItem} from "./schemas";
+
+mongoose.set("debug", true);
+
+mongoose.connect("mongodb://localhost:27017/users", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .catch((error) => console.log(error));
+
+// Create a new to-do list
+function createTodoList(listData) {
+    const list = new TodoList(listData);
+    const promise = list.save();
+    return promise;
+}
+
+// Read all to-do lists, or read a single to-do list by name
+function getTodoList(name) {
+    let promise;
+    if (name) {
+        promise = TodoList.find({name: name});
+    } else {
+        promise = TodoList.find();
+    }
+    return promise;
+
+}
+
+
+// Delete a to-do list by name
+function deleteTodoList(listName) {
+    // get the list then delete it
+    let promise;
+    promise = TodoList.delete({name: listName});
+    return promise;
+}
+
+// Create a new to-do item for a specific to-do list
+function createTodoItem(listName, itemData) {
+    let promise;
+    promise = TodoList.find({name: listName});
+    const item = new TodoItem(itemData);
+    promise = item.save();
+
+}
+
+
+// Delete a to-do item for a specific to-do list
+function deleteTodoItem(listName, task) {
+    let promise;
+    promise = TodoList.find({name: listName});
+    promise = TodoItem.delete({task: task});
+    return promise;
+
+}
+
--- achieveit-database/toDoItem.js
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-
-const Schema = new mongoose.Schema({
-    name: {}
-
-})
