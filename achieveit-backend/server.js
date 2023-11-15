de7c66641c8844520434ad715707c43345bd7ac0
Arctic1ce <davariartin@gmail.com>
2023-11-14 17:56:17 -0800

    changed Express port to use Azure port

--- achieveit-backend/server.js
+++ achieveit-backend/server.js
@@ -105,6 +105,6 @@ app.delete('/', async (req, res) => {
 });
 
 // Run the server
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`AchieveIt listening on port ${port}`);
 });
