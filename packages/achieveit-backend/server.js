72c2ac6795e408cc2da1b9466b565ad91043e21d
Arctic1ce <davariartin@gmail.com>
2023-11-15 16:50:59 -0800

    reverted broken changes

--- packages/achieveit-backend/server.js
+++ packages/achieveit-backend/server.js
@@ -1,10 +1,10 @@
 /* Filename: server.js */
 const express = require('express');
-/* const cors = require('cors'); */
+const cors = require('cors');
 const service = require('./achieveit-database/service');
 const app = express();
 const port = 8000;
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 /* Validate a to-do item */
@@ -40,13 +40,7 @@ app.get('/', async (req, res) => {
 app.post('/', async (req, res) => {
   try {
     const newList = req.query['name'];
-    const list = await service.getTodoList(newList);
-    const result;
-    if (list) {
-      result = list;
-    } else {
-      result = await service.createTodoList(newList);
-    }
+    result = await service.createTodoList(newList);
     res.send(result);
   } catch (error) {
     console.error(error);
