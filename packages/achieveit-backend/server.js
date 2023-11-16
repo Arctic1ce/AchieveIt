aa7311570a36eea3fbba6b8e042254f8491db210
Arctic1ce <davariartin@gmail.com>
2023-11-15 17:22:18 -0800

    uncommented cors

--- packages/achieveit-backend/server.js
+++ packages/achieveit-backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const service = require('./achieveit-database/service');
 const app = express();
 const port = 8000;
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 /* Validate a to-do item */
