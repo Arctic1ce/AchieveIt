833ae55cf0dd10dd5375a194483625967ca84b53
akhil <aelamanc@calpoly.edu>
2023-11-16 18:38:20 -0800

    input checking for Login and Signup

--- packages/achieveit-frontend/src/Signup.js
+++ packages/achieveit-frontend/src/Signup.js
@@ -1,46 +1,109 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Signup() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  });
+
+  const [formErrors, setFormErrors] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    setFormErrors({
+      ...formErrors,
+      [name]: value ? '' : `Please enter your ${name.toLowerCase()}`,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Check for empty fields
+    const errors = {};
+    for (const key in formData) {
+      if (!formData[key]) {
+        errors[key] = `Please enter your ${key.toLowerCase()}`;
+      }
+    }
+
+    // Update formErrors state
+    setFormErrors(errors);
+
+    // If there are no errors, proceed with form submission
+    if (Object.keys(errors).length === 0) {
+      // Perform form submission logic here
+      console.log('Form submitted:', formData);
+    }
+  };
+
   return (
     <div className="d-flex align-items-center justify-content-center" style={{ height: '100vh' }}>
       <div className="card" style={{ width: '400px' }}>
-        <form className="card-body">
+        <form className="card-body" onSubmit={handleSubmit}>
           <h3 className="text-center mb-4">Sign Up</h3>
 
           <div className="mb-3">
             <label className="form-label">First name</label>
             <input
               type="text"
-              className="form-control"
+              className={`form-control ${formErrors.firstName ? 'is-invalid' : ''}`}
               placeholder="First name"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleInputChange}
             />
+            {formErrors.firstName && <div className="invalid-feedback">{formErrors.firstName}</div>}
           </div>
 
           <div className="mb-3">
             <label className="form-label">Last name</label>
             <input
               type="text"
-              className="form-control"
+              className={`form-control ${formErrors.lastName ? 'is-invalid' : ''}`}
               placeholder="Last name"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleInputChange}
             />
+            {formErrors.lastName && <div className="invalid-feedback">{formErrors.lastName}</div>}
           </div>
 
           <div className="mb-3">
             <label className="form-label">Email address</label>
             <input
               type="email"
-              className="form-control"
+              className={`form-control ${formErrors.email ? 'is-invalid' : ''}`}
               placeholder="Enter email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
             />
+            {formErrors.email && <div className="invalid-feedback">{formErrors.email}</div>}
           </div>
 
           <div className="mb-3">
             <label className="form-label">Password</label>
             <input
               type="password"
-              className="form-control"
+              className={`form-control ${formErrors.password ? 'is-invalid' : ''}`}
               placeholder="Enter password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
             />
+            {formErrors.password && <div className="invalid-feedback">{formErrors.password}</div>}
           </div>
 
           <div className="d-grid">
