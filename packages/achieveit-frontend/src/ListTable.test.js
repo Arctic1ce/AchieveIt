7a5ecdb7ece7e69bb17ea71fb9edf2e70ab15ff5
Vibhu Garg <vibhugarg@Vibhus-MacBook-Pro.local>
2023-12-07 19:03:45 -0800

    frontend testing

--- packages/achieveit-frontend/src/ListTable.test.js
+++ packages/achieveit-frontend/src/ListTable.test.js
@@ -25,27 +25,68 @@ const mockProps = {
   deleteList: jest.fn(),
 };
 
-test('renders ListTable component', () => {
-  const { getByText } = render(<ListTable {...mockProps} />);
+
+test('renders ListTable component with specific list name', () => {
+  const { getByTestId, getByText } = render(<ListTable {...mockProps} listName="SampleList" />);
   
-  // Check if a specific text is present in the rendered component
-  expect(getByText('Task: Task1')).toBeInTheDocument();
-  
+  // Check if the specified list name is rendered
+  expect(getByTestId('list-name')).toHaveTextContent('SampleList');
+  expect(getByText('Task1')).toBeInTheDocument();
 });
 
-
-test('handles checkbox click', () => {
-  const { getByLabelText } = render(<ListTable {...mockProps} />);
-  const checkbox = getByLabelText('Task: Task1');
+test('handles checkbox click and calls setChecked', () => {
+  const { getByTestId } = render(<ListTable {...mockProps} />);
+  const checkbox = getByTestId('checkbox-SampleList-Task1');
   fireEvent.click(checkbox);
   expect(mockProps.setChecked).toHaveBeenCalledWith('SampleList', 'Task1', true);
 });
 
-test('handles delete button click', () => {
+
+test('handles delete button click and calls deleteTask', () => {
   const { getByText } = render(<ListTable {...mockProps} />);
   const deleteButton = getByText('Delete');
   fireEvent.click(deleteButton);
   expect(mockProps.deleteTask).toHaveBeenCalledWith('SampleList', 'Task1');
 });
 
+test('renders ListTable component with multiple lists', () => {
+  const multipleListProps = {
+    ...mockProps,
+    list: [
+      // Add another list to the existing mockProps.list
+      {
+        name: 'AnotherList',
+        items: [
+          {
+            name: 'Task2',
+            description: 'Description2',
+            due_date: '2023-01-02',
+            priority: 'Low',
+            completed: false,
+          },
+        ],
+      },
+      // Existing list from mockProps
+      {
+        name: 'SampleList',
+        items: [
+          {
+            name: 'Task1',
+            description: 'Description1',
+            due_date: '2023-01-01',
+            priority: 'High',
+            completed: false,
+          },
+        ],
+      },
+    ],
+  };
 
+  const { getByText } = render(<ListTable {...multipleListProps} />);
+  
+  // Check if both list names are rendered
+  expect(getByText('SampleList')).toBeInTheDocument();
+  expect(getByText('AnotherList')).toBeInTheDocument();
+});
+
+// Add more test cases as needed...
